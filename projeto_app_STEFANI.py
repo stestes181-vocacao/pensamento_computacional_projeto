71a4ef03518fabafc8e525e4ca035764ff336dbd
Stephanie_fanny-Vocação <stestes181@gmail.com>
2026-03-11 18:10:25 -0300

    Add files via upload

---
+++ projeto_app_STEFANI.py
@@ -0,0 +1,108 @@
+'''
+CRUD
+
+SALÃO DE BELEZA 
+
+
+
+
+
+
+'''
+
+
+print('\n === Sistema de Salão de Beleza === \n')
+
+print('1.Ver Agenda')
+print('2.Fazer agendamento')
+print('3.Escolher Serviço')
+print('4.Cancelar Agendamento')
+print('5.Mapa')
+print('6.Suporte')
+print('0.Sair')
+
+while True:
+
+    escolha_menu = input("\n escolha uma opçao: " )
+    if escolha_menu == '1':
+
+        print('Ver agenda...')
+        nome_cliente = input('digite o nome do cliente:')
+        telefone_cliente = ('digite o telefone do cliente:')
+
+    elif escolha_menu == '0':
+
+        print("Saindo do sistema. Ate logo!")
+        break
+
+
+    else:
+        print("Opçao invalida. Por favor, tente novmente.")
+
+print('\nSistema de Agendamento de Aulas - Meet\n')
+# print('')
+print(':: Programação Python::\n')
+# print('')
+print('Turma A Segunda e Quarta')
+print('Turma A 08:00 - 12:00\n')
+# print('')
+print('Turma A 10:00 - 12:00')
+
+
+print('\n Calculadora simples - Python Vocação\n')
+
+numb_hum = input('Digite o primeiro número:')
+numb_dois = input('Digite o segundo número:')
+# operar_numb = input('Escolha a operação: +, -, *, /')
+result = numb_hum + numb_dois
+
+print(f'O resultado é: {result}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+frutas = [ 'maçã','banana','laranja', 'abacaxi']
+
+frutas.append('amora')
+frutas.append('mirtilo')
+frutas.append('melancia')
+
+print(frutas[5])
+
+print('===GPS - Do Meu Amigo===')
+
+cores = ('preto','magenta', 'ciano', 'amarelo','verde')
+
+location 
